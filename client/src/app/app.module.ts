9d96d1450ef43e88528a364f15940e0d69122445
Cody Patnaude <cpatnaude@schoolfamilymedia.com>
2018-05-20 21:02:39 -0400

    added router and login

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,12 +4,15 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { ListService } from "./services/list.service";
 import { ListComponent } from "./list/list.component";
+import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [AppComponent, ListComponent],
+  declarations: [AppComponent, ListComponent, LoginComponent],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [ListService],
   bootstrap: [AppComponent]
